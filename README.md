fcdfd828c2160e8db7588032c397e33f72533ac6
Sabesh Bharathi <sabesh5601@gmail.com>
2020-07-07 10:45:16 +0530

    Added origin

---
+++ README.md
@@ -0,0 +1,5 @@
+# ViewsAndModifiers
+
+### 100DaysOfSwiftUI - Project 3
+
+A Technique project that explores various basic concepts of Views and Modifiers in SwiftUI. Oh I also made my own views and modifiers!! 🌚
--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,14 +8,100 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+    func watermarked(with text: String) -> some View {
+        self.modifier(Watermark(text: text))
+    }
+    func header() -> some View {
+        self.modifier(blueTitle())
     }
 }
+
+struct Watermark: ViewModifier {
+    var text: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(Color.black)
+        }
+    }
+}
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 100))
+    }
+}
+
+struct blueTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+    }
+}
+
+
+struct GridStack<Content: View>: View {
+    
+    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
+        self.rows = rows
+        self.columns = columns
+        self.content = content
+    }
+    
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<self.columns, id: \.self) { column in
+                        self.content(row, column)
+                    
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("Bruhhh")
+                .header()
+            Spacer()
+            GridStack(rows: 4, columns: 4) { row, col in
+               Image(systemName: "\(row * 4 + col).circle")
+               Text("R\(row) C\(col)")
+            }
+            Spacer()
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        .previewDevice(PreviewDevice(rawValue: "iPhone XR"))
     }
 }
